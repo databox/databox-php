774bfd9edb86c7860f17acc15fbc359f077cee76
Oto Brglez <otobrglez@gmail.com>
2015-06-30 16:05:16 +0200

    Adding basic stuff

--- src/Databox/Client.php
+++ src/Databox/Client.php
@@ -1,6 +1,58 @@
 <?php
 
-class Client {
+namespace Databox;
 
-};
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Event\BeforeEvent;
 
+class Client extends GuzzleClient
+{
+
+    function __construct($pushToken = null)
+    {
+        parent::__construct([
+            'base_url' => 'https://push2new.databox.com',
+            'defaults' => [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ],
+                'auth' => [$pushToken, '', 'Basic']
+            ],
+        ]);
+
+        /*
+        $this->getEmitter()->on('before', function (BeforeEvent $e) {
+            echo PHP_EOL.'About to send request: ' . $e->getRequest();
+        });
+        */
+    }
+
+    private function processKPI($key, $value, $date = null)
+    {
+        $data = [sprintf("$%s", $key) => $value];
+        if (!is_null($date)) $data["date"] = $date;
+        return $data;
+    }
+
+    public function push($key, $value, $date = null)
+    {
+        return $this->post('/', [
+            'json' => ['data' => [$this->processKPI($key, $value, $date)]]
+        ])->json()["status"] == "ok";
+    }
+
+    public function insertAll($kpis)
+    {
+        return $this->post('/', [
+            'json' => ['data' => array_map(function ($i) {
+                if (count($i) < 3) $i[] = null;
+                return $this->processKPI($i[0], $i[1], $i[2]);
+            }, $kpis)]
+        ])->json()["status"] == "ok";
+    }
+
+    public function lastPush($n = 1)
+    {
+        return $this->post(sprintf('/lastpushes/%d', $n))->json();
+    }
+}
--- tests/Databox/Tests/ClientTest.php
+++ tests/Databox/Tests/ClientTest.php
@@ -2,12 +2,37 @@
 
 namespace Databox\Tests;
 
-use Databox\Client as Client;
+use Databox\Client;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
-    public function testInitialisation(){
-        $this->client = new Client();
-        $this->assertEquals(get_class($this->client), Client);
+    function __construct(){
+        $this->client = new Client("adxg1kq5a4g04k0wk0s4wkssow8osw84");
+    }
+
+    /*
+    public function testInitialisation()
+    {
+        //TODO: Token thing here
+        $this->assertEquals(get_class($this->client), 'Databox\Client');
+    }
+*/
+
+    public function testPush() {
+        $this->assertTrue($this->client->push("sales", 53.2));
+        $this->assertTrue($this->client->push("sales", 40, "2015-01-01 17:00:00"));
+    }
+
+    public function testInsertAll() {
+        $this->assertTrue($this->client->insertAll([
+            ["sales", 53.2],
+            ["sales", 50.2, "2015-01-01 17:00:00"],
+        ]));
+    }
+
+
+    public function testLastPush() {
+        $this->assertCount(1, $this->client->lastPush());
+        $this->assertCount(3, $this->client->lastPush(3));
     }
 }
